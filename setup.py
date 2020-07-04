6fd0e77f3c755116b1e89788dd57ab239c2444b8
Mark Jessop <vk5qi@rfhead.net>
2020-07-04 20:12:30 +0930

    Fix setup.py, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 regexp = re.compile(r".*__version__ = [\'\"](.*?)[\'\"]", re.S)
 
-init_file = os.path.join(os.path.dirname(__file__), "src", "horusdemodlib", "__init__.py")
+init_file = os.path.join(os.path.dirname(__file__), "horusdemodlib", "__init__.py")
 with open(init_file, "r") as f:
     module_content = f.read()
     match = regexp.match(module_content)
@@ -34,8 +34,8 @@ if __name__ == "__main__":
         version=version,
         install_requires=requirements,
         keywords=["horus modem telemetry radio"],
-        package_dir={"": "src"},
-        packages=find_packages("src"),
+        package_dir={"": "."},
+        packages=find_packages("."),
         classifiers=[
             "Intended Audience :: Developers",
             "Programming Language :: Python :: 3.6",
