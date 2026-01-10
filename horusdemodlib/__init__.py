78a1a66942b2931d9516e7f4459e90527380c8f3
xssfox <xss@sprocketfox.io>
2026-01-11 08:32:47 +1100

    make gha action envs work, make version only be settable in a single location

--- horusdemodlib/__init__.py
+++ horusdemodlib/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.5.1"
+import importlib.metadata
+try:
+    __version__ = importlib.metadata.version(__package__)
+except:
+    # during build the package is imported before being installed so we don't have metadata yet
+    __version__ = 'unknown'
