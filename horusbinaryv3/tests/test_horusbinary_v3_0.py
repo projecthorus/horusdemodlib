9c19b48325018a690e39a390baac886862eec2eb
xssfox <xss@sprocketfox.io>
2026-03-14 20:55:34 +1100

    add backwards compatability test

--- horusbinaryv3/tests/test_horusbinary_v3_0.py
+++ horusbinaryv3/tests/test_horusbinary_v3_0.py
@@ -4,9 +4,126 @@ import logging
 import json
 import datetime
 
+PRE_EXTENSION="""
+-- auto tags because this will be used in uper anyway where tags don't get used (order is important though)
+HorusBinaryV3 DEFINITIONS AUTOMATIC TAGS ::= BEGIN
+
+BitFlags ::= SEQUENCE {
+  b0  BOOLEAN,
+  b1  BOOLEAN,
+  b2  BOOLEAN,
+  b3  BOOLEAN,
+  b4  BOOLEAN,
+  b5  BOOLEAN,
+  b6  BOOLEAN,
+  b7  BOOLEAN
+}
+
+CustomFieldValues ::= CHOICE {
+  -- prefixed with horus otherwise c codegen is bad
+
+  -- allows base64 - if you were crazy enough, but really you should use customData instead
+  horusStr   IA5String (FROM("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_ +/=-.")^SIZE (0..255)), 
+  horusInt   SEQUENCE(SIZE(1..4)) OF INTEGER,
+  horusReal  SEQUENCE(SIZE(1..4)) OF REAL,
+
+  -- we could use a bit string here, but it packs down the same so we may as well make it easier on ourselves to process
+  horusBool  BitFlags
+}
+
+AdditionalSensorType ::= SEQUENCE {
+  -- adding a name will add 
+  -- overhead but means anyone can find out what the sensor does
+  name    IA5String (FROM("abcdefghijklmnopqrstuvwxyz0123456789-")^SIZE (1..20)) OPTIONAL, 
+  
+  -- values are optional as we may want to send a packet without values and only names
+  values  CustomFieldValues OPTIONAL
+}
+
+-- we limit to 4 additional sensor types to lower our used bits
+AdditionalSensors ::= SEQUENCE(SIZE(1..4)) OF AdditionalSensorType
+
+TemperatureSensors ::= SEQUENCE {
+  internal   INTEGER (-1023..1023) OPTIONAL,  -- tenths C
+  external   INTEGER (-1023..1023) OPTIONAL,  -- tenths C
+  custom1    INTEGER (-1023..1023) OPTIONAL, -- tenths C
+  custom2    INTEGER (-1023..1023) OPTIONAL  -- tenths C
+}
+
+MilliVoltSensors ::= SEQUENCE {
+  battery    INTEGER (0..16383) OPTIONAL, -- millivolts
+  solar      INTEGER (0..16383) OPTIONAL, -- millivolts
+  custom1    INTEGER (0..16383) OPTIONAL, -- millivolts
+  custom2    INTEGER (0..16383) OPTIONAL -- millivolts
+}
+
+-- u-blox power save state
+GnssPowerSaveState   ::= ENUMERATED {
+  psmNotActive    (0),
+  enabled         (1),
+  acquisition     (2),
+  tracking        (3),
+  optimised       (4),
+  inactive        (5)
+}
+
+Via ::= ENUMERATED {
+  sondehub (0),
+  nohub    (1),
+  unknown  (2),
+  unknown  (3),
+  unknown  (4),
+  unknown  (5),
+  unknown  (6),
+  unknown  (7)
+}
+
+Telemetry ::= SEQUENCE {
+  -- required fields
+  payloadCallsign     IA5String (FROM("-/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")^SIZE (1..15)),
+  sequenceNumber      INTEGER (0..65535),
+
+  -- these are common fields that are required to transmit
+  -- however if the data is missing, transmit 0,0 for lat/lon and timeofdayseconds -1
+  timeOfDaySeconds    INTEGER (-1..86400), -- seconds since
+                                                              -- 00:00 UTC - highly recommended to have time of day seconds in every transmission
+  latitude            INTEGER (-9000000..9000000),    -- fixed point, x100000
+  longitude           INTEGER (-18000000..18000000),  -- fixed point, x100000
+
+  -- highly recommended field
+  altitudeMeters      INTEGER (-1000..50000), -- meters - set to -1000 if invalid
+
+  -- additional extra sensors
+  extraSensors        AdditionalSensors OPTIONAL,
+
+  -- additional fields that would be unlikely to need to be sequences
+  velocityHorizontalKilometersPerHour  INTEGER (0..512)         OPTIONAL, -- km/hr
+  gnssSatellitesVisible                INTEGER (0..31)          OPTIONAL,
+  ascentRateCentimetersPerSecond       INTEGER (-32767..32767)  OPTIONAL, -- cm/s
+  pressurehPa-x10                      INTEGER (0..12000)        OPTIONAL, -- hPa * 10
+
+
+  -- additional but fairly common fields so we make them explicit
+  temperatureCelsius-x10 TemperatureSensors              OPTIONAL, -- C
+  humidityPercentage     INTEGER (0..100)                OPTIONAL, -- %
+  milliVolts             MilliVoltSensors                OPTIONAL, -- 
+  counts                 SEQUENCE(SIZE(1..8)) OF INTEGER OPTIONAL,
+  gnssPowerSaveState     GnssPowerSaveState              OPTIONAL,
+
+  -- binary data
+  customData           OCTET STRING(SIZE (0..255)) OPTIONAL,
+  ...  -- this is an extension marker - it lets us add fields
+}
+END
+
+
+"""
+
+
 class TestHorusBinaryV3_0(unittest.TestCase):
     def setUp(self):
         self.uper = asn1tools.compile_files("./HorusBinaryV3.asn1", codec="uper")
+        self.uper_pre = asn1tools.compile_string(PRE_EXTENSION, codec="uper")
         self.maxDiff = None
 
     # This tests building a payload with practically every feature
@@ -64,6 +181,63 @@ class TestHorusBinaryV3_0(unittest.TestCase):
 
         logging.debug(f"Length: {len(encoded)} Data: {encoded.hex()}")
 
+        self.assertDictEqual(data, decoded)
+
+    def test_bells_and_whistles_pre(self): # tests decoding with the pre decoder even if extensions used
+        
+        data = { # This is an example packet that will be way too big, but it highlights all the features
+            "payloadCallsign": "abcDEF-0123abc-",
+            "sequenceNumber": 65535,
+            "timeOfDaySeconds": 86400,
+            "latitude": 9000000,
+            "longitude": -18000000,
+            "altitudeMeters": 50000,
+            "velocityHorizontalKilometersPerHour": 255,
+            "gnssSatellitesVisible": 31,
+            "ascentRateCentimetersPerSecond": 32767,
+            "pressurehPa-x10": 1270,
+            "temperatureCelsius-x10": {
+                "internal": -127,
+                "external": 127,
+                "custom1": -127,
+                "custom2": 127,
+                
+            },
+            "humidityPercentage":100,
+            "milliVolts": {
+                "battery": 0,
+                "solar": 16383,
+                "custom1": 0,
+                "custom2": 16383,
+                },
+            "counts": [1,100,1000,10000,100000,1000000,1000000],
+            "gnssPowerSaveState": "tracking",
+
+            "extraSensors": [
+                {
+                    "name": "hbk8359hbk8359hbk835", 
+                    "values": ("horusInt", [1,2]) 
+                },
+                {
+                    "name": "crm114",
+                    "values": ("horusBool", {
+                        "b0": True,"b1": True,"b2": True,"b3": True,"b4": True,"b5": True,"b6": True,"b7": True
+                    })
+                },
+                { 
+                    "values": ("horusStr", "ABCEDFGEFGH123324asdbkjbsdg")
+                },
+                {
+                    "values": ("horusReal",[0.1234,1232342345234234,0.1234,1232342345234234])
+                }
+            ],
+            "via": "nohub"
+        }
+        encoded = self.uper.encode("Telemetry",data)
+        decoded = self.uper_pre.decode("Telemetry", encoded)
+
+        logging.debug(f"Length: {len(encoded)} Data: {encoded.hex()}")
+        data.pop('via', None)
         self.assertDictEqual(data, decoded)
 
     def test_bells_and_whistles_extend(self):
