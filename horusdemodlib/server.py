91368fecc042adc1487aa9c4fd10471ae0670134
xssfox <xss@sprocketfox.io>
2026-06-24 12:08:26 +1000

    Combine with uploaded and add in metadata

--- horusdemodlib/server.py
+++ horusdemodlib/server.py
@@ -13,6 +13,14 @@ import json
 from .demod import Mode, Frame, HorusLib
 import socket 
 import select
+import traceback
+from .demodstats import FSKDemodStats
+import time
+from .payloads import init_custom_field_list, init_payload_id_list
+from .sondehubamateur import *
+from .uploader import read_config
+from .decoder import decode_packet, parse_ukhas_string
+
 
 horus_api = _horus_api_cffi.lib
 
@@ -28,78 +36,231 @@ READ_WRITE = READ_ONLY | select.POLLOUT
 class HorusTCPInstance:
     def __init__(self,
              connection,
-             mode,
              args,
-             fout   
+             fout,
+             sondehub_uploader,
+             logfile
              ):
-        self.h = HorusLib(mode=mode,tone_spacing=args.tonespacing, stereo_iq=args.q, verbose=int(args.v), callback=self.frame_callback, sample_rate=args.sample_rate, rate=int(args.rate))
-        _decoder_info = f"Starting {args.mode} decoder, {args.rate} baud, {f'{args.tonespacing} Hz Tone Spacing, ' if args.tonespacing>0 else ''} {args.sample_rate} Hz sample rate {'IQ' if args.q else ''}"
-        logging.info(_decoder_info)
-        if args.fsk_lower > -99999 and args.fsk_upper > args.fsk_lower:
-            self.h.set_estimator_limits(args.fsk_lower, args.fsk_upper)
-            logging.info(f"Frequency Estimator Limits set to {args.fsk_lower}-{args.fsk_upper} Hz.")
-
-
         
-        self.stats_counter = args.stats
+
+        self.h = None
         self.args = args
         self.buffer = b''
         self.fout = fout
         self.connection = connection
+        self.freq_hz = None
+        self.freq_target_hz = None
+        self.demod_stats = None
+        self.sondehub_uploader = sondehub_uploader
+        self.logfile = logfile
+
+        # Some variables to handle re-downloading of payload ID lists.
+        self.min_download_time = 30*60 # Only try and download new payload ID / custom field lists every 30 min.
+        self.next_download_time = time.time()
+
+    def configure(self, data):
+        configuration_data = json.loads(data.strip())
+
+        if configuration_data['mode'].lower() == 'rtty7n2' or configuration_data['mode'].lower() == 'rtty':
+            mode = Mode.RTTY_7N2
+        elif configuration_data['mode'].lower() == 'rtty7n1':
+            mode = Mode.RTTY_7N1
+        elif configuration_data['mode'].lower() == 'rtty8n2':
+            mode = Mode.RTTY_8N2
+        elif configuration_data['mode'].lower() == 'binary':
+            mode = Mode.BINARY
+        else:
+            logging.warning(f"Unknown mode {configuration_data['mode'].lower()}. Assuming horus binary.")
+            mode = Mode.BINARY
+
+        if 'tonespacing' in configuration_data:
+            tonespacing = configuration_data['tonespacing']
+        else:
+            tonespacing = -1
+
+        if "iq"  in configuration_data:
+            iq = configuration_data["iq"]
+        else:
+            iq = False
+        
+        if "samplerate" in configuration_data:
+            samplerate = configuration_data["samplerate"]
+        else:
+            samplerate = 48000
+
+        if "modemrate" in configuration_data:
+            modemrate = configuration_data["modemrate"]
+        else:
+            modemrate = 100
+        self.modemrate = modemrate
+
+        if "freq_hz" in configuration_data:
+            self.freq_hz = configuration_data["freq_hz"]
+        if "freq_target_hz" in configuration_data:
+            self.freq_target_hz = configuration_data["freq_target_hz"]
+
+        self.h = HorusLib(mode=mode,tone_spacing=tonespacing, stereo_iq=iq, verbose=int(self.args.v), callback=self.frame_callback, sample_rate=samplerate, rate=modemrate)
+        _decoder_info = f"Starting {mode} decoder, {modemrate} baud, {f'{tonespacing} Hz Tone Spacing, ' if tonespacing>0 else ''} {samplerate} Hz sample rate {'IQ' if iq else ''}"
+        logging.info(_decoder_info)
+
+
+        if (
+            "fsk_upper" in configuration_data and "fsk_lower" in configuration_data and
+            configuration_data["fsk_lower"] > -99999 and configuration_data["fsk_upper"] > configuration_data["fsk_lower"]
+        ):
+            self.h.set_estimator_limits(configuration_data["fsk_lower"], configuration_data["fsk_upper"])
+            logging.info(f"Frequency Estimator Limits set to {configuration_data['fsk_lower']}-{configuration_data['fsk_upper']} Hz.")
+
+        self.demod_stats = FSKDemodStats(peak_hold=True)
 
     def close(self):
-        self.h.close()
+        if self.h:
+            self.h.close()
         self.connection.close()
 
     def write(self,data):
+        if not self.h:
+            self.configure(data)
+            return
+        
         self.buffer += data
 
 
 
         output = self.h.add_samples(data)
-        if self.args.v:
-            if output:
-                sys.stderr.write(f"Sync: {output.sync}  SNR: {output.snr}\n")
-        if self.args.stats != None:
-            stats_out = {
-                "EbNodB": self.h.stats.snr_est,
-                "ppm": self.h.stats.clock_offset,
-                "f1_est": self.h.stats.f_est[0],
-                "f2_est": self.h.stats.f_est[1]
-            }
+        # if self.args.v:
+        #     if output:
+        #         sys.stderr.write(f"Sync: {output.sync}  SNR: {output.snr}\n")
+        # stats_out = {
+        #     "EbNodB": self.h.stats.snr_est,
+        #     "ppm": self.h.stats.clock_offset,
+        #     "f1_est": self.h.stats.f_est[0],
+        #     "f2_est": self.h.stats.f_est[1]
+        # }
+    
+        # if self.h.mfsk == 4:
+        #     stats_out["f3_est"] = self.h.stats.f_est[2]
+        #     stats_out["f4_est"] = self.h.stats.f_est[3]
         
-            if self.h.mfsk == 4:
-                stats_out["f3_est"] = self.h.stats.f_est[2]
-                stats_out["f4_est"] = self.h.stats.f_est[3]
-            
-            eye_diagram = []
-            for i in range(self.h.stats.neyetr):
-                eye_diagram.append([])
-                for j in range(self.h.stats.neyesamp):
-                    eye_diagram[i].append(self.h.stats.rx_eye[i][j])
-            stats_out['eye_diagram'] = eye_diagram
-            stats_out['samp_fft']=[0]*128 # broken in horus_demod.c - replicating the same output
-            if self.args.g:
-                print(json.dumps(stats_out))
-            else:
-                sys.stderr.write(json.dumps(stats_out)+"\n")
+        # eye_diagram = []
+        # for i in range(self.h.stats.neyetr):
+        #     eye_diagram.append([])
+        #     for j in range(self.h.stats.neyesamp):
+        #         eye_diagram[i].append(self.h.stats.rx_eye[i][j])
+        # stats_out['eye_diagram'] = eye_diagram
+        # stats_out['samp_fft']=[0]*128 # broken in horus_demod.c - replicating the same output
+          
 
     def frame_callback(self, frame):
         # Print out only CRC-passing frames, unless we are in verbose mode
         if frame.crc_pass or self.args.v:
-
             if type(frame.data) == bytes:
                 self.fout.write(frame.data.hex().upper())
             else:
                 self.fout.write(frame.data)
         
-            if self.args.c:
-                if frame.crc_pass:
-                    self.fout.write(f"  CRC OK")
-                else:
-                    self.fout.write(f"  CRC BAD")
             self.fout.write("\n")
             self.fout.flush()
+
+        if frame.crc_pass:
+            if frame.data.startswith(b'$$'):
+                data = frame.data.decode()
+                # RTTY packet handling.
+                # Attempt to extract fields from it:
+                logging.info(f"Received raw RTTY packet: {frame.data}")
+                try:
+                    _decoded = parse_ukhas_string(data)
+                    # If we get here, the string is valid!
+
+                    # Add in SNR data.
+                    _snr = self.demod_stats.snr
+                    _decoded['snr'] = _snr
+
+                    # Add in frequency estimate, if we have been supplied a receiver frequency.
+                    if self.freq_hz:
+                        _decoded['f_centre'] = int(self.demod_stats.fest_mean) + int(self.freq_hz)
+
+                    # Add in tone spacing estimate.
+                    if self.demod_stats.fest_spacing > 0.0:
+                        _decoded['tone_spacing'] = int(self.demod_stats.fest_spacing)
+
+                    # Add in baud rate, if provided.
+                    _decoded['baud_rate'] = int(self.modemrate)
+
+
+                    # Logfile string
+                    _decoded_str = "$$" + data.split('$')[-1] + '\n'
+
+                    # Upload the string to Sondehub Amateur
+                    self.sondehub_uploader.add(_decoded)
+
+                    if self.logfile:
+                        self.logfile.write(_decoded_str)
+                        self.logfile.flush()
+
+                    logging.info(f"Decoded String (SNR {self.demod_stats.snr:.1f} dB): {_decoded_str[:-1]}")
+
+                except Exception as e:
+                    logging.error(f"Decode Failed: {traceback.format_exc()}")
+
+
+            else:
+                # Handle binary packets
+                logging.info(f"Received raw binary packet: {frame.data.hex()}")
+
+                try:
+                    _decoded = decode_packet(frame.data)
+                    # If we get here, we have a valid packet!
+
+                    if (_decoded['callsign'] == "UNKNOWN_PAYLOAD_ID"):
+                        # We haven't seen this payload ID. Our payload ID list might be out of date.
+                        if time.time() > next_download_time:
+                            logging.info("Observed unknown Payload ID, attempting to re-download lists.")
+                            
+                            # Download lists.
+                            horusdemodlib.payloads.HORUS_PAYLOAD_LIST = init_payload_id_list(filename="payload_id_list.txt")
+                            horusdemodlib.payloads.HORUS_CUSTOM_FIELDS = init_custom_field_list(filename="custom_field_list.json")
+                            
+                            # Update next_download_time so we don't re-attempt to download with every new packet.
+                            next_download_time = time.time() + self.min_download_time
+
+                            # Re-attempt to decode the packet.
+                            _decoded = decode_packet(frame.data)
+                            if _decoded['callsign'] != "UNKNOWN_PAYLOAD_ID":
+                                logging.info(f"Payload found in new payload ID list - {_decoded['callsign']}")
+                    
+                    # Add in SNR data.
+                    _snr = self.demod_stats.snr
+                    _decoded['snr'] = _snr
+
+                    # Add in frequency estimate, if we have been supplied a receiver frequency.
+                    if self.freq_hz:
+                        _decoded['f_centre'] = int(self.demod_stats.fest_mean) + int(self.freq_hz)
+
+                    # Add in tone spacing estimate.
+                    if self.demod_stats.fest_spacing > 0.0:
+                        _decoded['tone_spacing'] = int(self.demod_stats.fest_spacing)
+
+                    # Add in baud rate, if provided.
+                    _decoded['baud_rate'] = int(self.modemrate)
+
+                    # Upload the string to Sondehub Amateur
+                    self.sondehub_uploader.add(_decoded)
+
+                    if self.logfile:
+                        self.logfile.write(_decoded['ukhas_str']+'\n')
+                        self.logfile.flush()
+
+                    logging.info(f"Decoded Binary Packet (SNR {self.demod_stats.snr:.1f} dB): {_decoded['ukhas_str']}")
+                    # Remove a few fields from the packet before printing.
+                    _temp_packet = _decoded.copy()
+                    _temp_packet.pop('packet_format')
+                    _temp_packet.pop('ukhas_str')
+                    logging.debug(f"Binary Packet Contents: {_temp_packet}")
+                
+                except Exception as e:
+                    logging.error(f"Decode Failed: {e}")
+                    logging.debug(f"Traceback: {traceback.format_exc()}")
 
 
 def main():
@@ -108,30 +269,17 @@ def main():
                     description='Receives audio stream via TCP and demodulates the frames')    
 
     modes = ["RTTY","RTTY7N1","RTTY8N2","RTTY7N2","BINARY"]
-    parser.add_argument('-m','--mode',choices=modes+[x.lower() for x in modes], default="binary", help="RTTY or binary Horus protocol")
-    parser.add_argument('--sample-rate',default=48000, type=int,help="Audio sample rate")
-    parser.add_argument('--rate',default=100, type=int,help="Customise modem baud rate. Default: (depends on mode)")
+    parser.add_argument('-c', '--config', type=str, default='user.cfg', help="Configuration file to use. Default: user.cfg")
     parser.add_argument('--tcp-port',default=4921, type=int,help="TCP port to listen for samples")
-    parser.add_argument('--tonespacing',default=-1, type=int,help="Transmitter Tone Spacing (Hz) Default: Not used.")
-    parser.add_argument('-t','--stats', default=None,  nargs='?', const=8, type=int, help="Print out modem statistics to stderr in JSON") # TODO 
     parser.add_argument('-g', action="store_true", default=False,help="Emit Stats on stdout instead of stderr")
-    parser.add_argument('-q', action="store_true",default=False,help="use stereo (IQ) input")
     parser.add_argument('-v', action="store_true",default=False,help="verbose debug info")
-    parser.add_argument('-c', action="store_true",default=False,help="display CRC results for each packet")
-    parser.add_argument('-u',"--fsk_upper", type=int, action="store",default=False,help="Estimator FSK upper limit")
-    parser.add_argument('-b',"--fsk_lower", type=int, action="store",default=False,help="Estimator FSK lower limit")
+    parser.add_argument("--log", type=str, default="telemetry.log", help="Write decoded telemetry to this log file.")
     parser.add_argument('output',nargs='?',action='store', default=sys.stdout, help="Output filename")
+    parser.add_argument("--noupload", action="store_true", default=False, help="Disable SondeHub upload.")
 
     args = parser.parse_args()
 
-    if args.mode.lower() == 'rtty7n2' or args.mode.lower() == 'rtty':
-        mode = Mode.RTTY_7N2
-    elif args.mode.lower() == 'rtty7n1':
-        mode = Mode.RTTY_7N1
-    elif args.mode.lower() == 'rtty8n2':
-        mode = Mode.RTTY_8N2
-    else:
-        mode = Mode.BINARY
+
 
 
     if type(args.output) == type(sys.stdout) or args.output == "-":
@@ -152,6 +300,48 @@ def main():
 
     logging.info(f"horusdemodlib v{horusdemodlib.__version__} - horus_demod")
 
+
+    if args.log != "none":
+        _logfile = open(args.log, 'a')
+        logging.info(f"Opened log file {args.log}.")
+    else:
+        _logfile = None
+
+
+    # Read in the configuration file.
+    user_config = read_config(args.config)
+
+    # If we could not read the configuration file, exit.
+    if user_config == None:
+        logging.critical(f"Could not load {args.config}, exiting...")
+        sys.exit(1)
+
+
+
+    horusdemodlib.payloads.HORUS_PAYLOAD_LIST = init_payload_id_list(filename="payload_id_list.txt")
+    horusdemodlib.payloads.HORUS_CUSTOM_FIELDS = init_custom_field_list(filename="custom_field_list.json")
+    logging.info(f"Payload list contains {len(list(horusdemodlib.payloads.HORUS_PAYLOAD_LIST.keys()))} entries.")
+    logging.info(f"Custom Field list contains {len(list(horusdemodlib.payloads.HORUS_CUSTOM_FIELDS.keys()))} entries.")
+
+
+    if user_config['station_lat'] == 0.0 and user_config['station_lon'] == 0.0:
+        _sondehub_user_pos = None
+    else:
+        _sondehub_user_pos = [user_config['station_lat'], user_config['station_lon'], 0.0]
+
+
+
+    sondehub_uploader = SondehubAmateurUploader(
+        upload_rate = 2,
+        user_callsign = user_config['user_call'],
+        user_position = _sondehub_user_pos,
+        user_radio = user_config['radio_comment'],
+        user_antenna = user_config['antenna_comment'],
+        software_name = "horusdemodlib",
+        software_version = horusdemodlib.__version__,
+        inhibit=args.noupload
+    )
+    logging.info("Using User Callsign: %s" % user_config['user_call'])
 
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -174,16 +364,22 @@ def main():
                     connection, client_address = p.accept()
                     logging.info(f"New client connected: {client_address}")
                     connection.setblocking(0)
-                    fd_to_socket[ connection.fileno() ] = HorusTCPInstance(connection=connection, mode=mode,args=args, fout=fout)
+                    fd_to_socket[ connection.fileno() ] = HorusTCPInstance(connection=connection,args=args, fout=fout, sondehub_uploader=sondehub_uploader, logfile=_logfile)
                     poller.register(connection, READ_ONLY)
                     
                 else:
                     data = p.connection.recv(1024)
                     if data:
-                        p.write(data)
-                        # Add output channel for response
-                        poller.modify(p.connection, READ_WRITE)
-
+                        try:
+                            p.write(data)
+                            # Add output channel for response
+                            poller.modify(p.connection, READ_WRITE)
+                        except:
+                            if p.h:
+                                logging.error(f"Error trying to process audio: {traceback.format_exc()}")
+                            else:
+                                logging.error(f"Error trying to configure horus: {traceback.format_exc()}")
+                            p.close()
                     else:
                         logging.info(f"Connection with {client_address} lost")
                         # Stop listening for input on the connection
